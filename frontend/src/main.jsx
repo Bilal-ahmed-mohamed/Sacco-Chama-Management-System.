7410b8dd8cb2141bb29c20c92f29666ec33c735a
bilal_ahmed <105801544+Bilal-ahmed-mohamed@users.noreply.github.com>
2025-11-05 10:20:52 +0300

    sorted an import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,7 +20,7 @@ import Reminder from './pages/Admin/Reminder.jsx';
 import AllContribution from './pages/Admin/AllContribution.jsx';
 import { AuthContextProvider } from '../context/AuthContext.jsx';
 import ProtectedRoute from './components/ProtectedRoutes.jsx';
-import AllLoans from './pages/Admin/Allloans.jsx';
+import AllLoans from './pages/Admin/AllLoans.jsx';
 import ContributionAdmin from './pages/Admin/ContributionAdmin.jsx';
 
 createRoot(document.getElementById('root')).render(
